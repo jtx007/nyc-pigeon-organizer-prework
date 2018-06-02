6f22558b3226e702c078f70d331b2345db6eb14e
James Thomas <jamesjacobthomas7@gmail.com>
2018-06-02 21:40:06 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |pigeon_attr, atrr_hash|
+    atrr_hash.each do |atrribute, array|
+      array.each do |name|
+        pigeon_list[name] = [atrribute.to_s]
+    
+    
+  binding.pry
+      end
+    end
+  end
 end
